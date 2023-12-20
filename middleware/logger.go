894f4b76e61f25561476a84316261bc0d1ab18bc
lucas <1783022886@qq.com>
2023-12-21 01:42:15 +0800

    log fix

--- middleware/logger.go
+++ middleware/logger.go
@@ -4,12 +4,6 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"net"
-	"net/http"
-	"net/http/httputil"
-	"os"
-	"runtime/debug"
-	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -33,7 +27,7 @@ func (bl BodyLog) Write(b []byte) (int, error) {
 	return bl.ResponseWriter.Write(b)
 }
 
-func LoggerMiddleware() gin.HandlerFunc {
+func RequestMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		traceID := utils.GetString(c.Value(consts.ContextTraceID))
 		if traceID != "" {
@@ -53,6 +47,11 @@ func LoggerMiddleware() gin.HandlerFunc {
 		if maxLen > requestBodyMaxLen {
 			maxLen = requestBodyMaxLen
 		}
+		path := c.Request.URL.Path
+		raw := c.Request.URL.RawQuery
+		if raw != "" {
+			path = path + "?" + raw
+		}
 		requestBodyLogBytes = make([]byte, maxLen)
 		copy(requestBodyLogBytes, requestBodyBytes)
 		if maxLen < len(requestBodyBytes) {
@@ -67,61 +66,15 @@ func LoggerMiddleware() gin.HandlerFunc {
 			"agent", c.Request.UserAgent(),
 			"body", string(requestBodyLogBytes),
 			"client_ip", c.ClientIP(),
-			"path", c.Request.URL.RawPath)
+			"path", path)
 
 		c.Next()
 
 		logger.Info("[GIN] response",
 			"error_message", c.Errors.ByType(gin.ErrorTypePrivate).String(),
 			"body", bodyLog.body.String(),
-			"path", c.Request.URL.RawPath,
+			"path", path,
 			"status_code", c.Writer.Status(),
 			"cost", fmt.Sprintf("%dms", time.Now().Sub(start).Milliseconds()))
-	}
-}
-
-// RecoveryMiddleware recover项目可能出现的panic，并使用zap记录相关日志
-func RecoveryMiddleware(stack bool) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		logger := log.GetCurrentGormLogger(c)
-		defer func() {
-			if err := recover(); err != nil {
-				// Check for a broken connection, as it is not really a
-				// condition that warrants a panic stack trace.
-				var brokenPipe bool
-				if ne, ok := err.(*net.OpError); ok {
-					if se, ok := ne.Err.(*os.SyscallError); ok {
-						if strings.Contains(strings.ToLower(se.Error()), "broken pipe") ||
-							strings.Contains(strings.ToLower(se.Error()), "connection reset by peer") {
-							brokenPipe = true
-						}
-					}
-				}
-
-				httpRequest, _ := httputil.DumpRequest(c.Request, false)
-				if brokenPipe {
-					logger.Error(c.Request.URL.Path,
-						"error", err,
-						"request", string(httpRequest))
-					// If the connection is dead, we can't write a status to it.
-					c.Error(err.(error)) // nolint: err check
-					c.Abort()
-					return
-				}
-
-				if stack {
-					logger.Error("[Recovery from panic]",
-						"error", err,
-						"request", string(httpRequest),
-						"stack", string(debug.Stack()))
-				} else {
-					logger.Error("[Recovery from panic]",
-						"error", err,
-						"request", string(httpRequest))
-				}
-				c.AbortWithStatus(http.StatusInternalServerError)
-			}
-		}()
-		c.Next()
 	}
 }
