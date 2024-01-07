be3a0fddbeed2c72208ca4c53a9ec358a9ce1112
zhouzhigao01 <zhouzhigao01@baidu.com>
2024-01-08 01:45:10 +0800

    redis延迟队列

--- redisx/extra.go
+++ redisx/extra.go
@@ -1,6 +1,13 @@
 package redisx
 
-import "context"
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+)
+
+const (
+	CacheKeyWithDelayQueueKey = "potato:__delay_queue__"
+)
 
 // PublishBody 消息结构体
 type PublishBody struct {
@@ -10,6 +17,17 @@ type PublishBody struct {
 	PlayLoad  []byte `msgpack:"4"` // 消息体
 	Timestamp int64  `msgpack:"5"` // 消息投递时间
 	Extra     string `msgpack:"6"` // 辅助消息
+}
+
+// Watch 监听
+func (op *RedisOperator) Watch(ctx context.Context, fn func(*redis.Tx) error, keys ...string) (err error) {
+	return op.client.Watch(ctx, fn, keys...)
+}
+
+// migrateExpiredBody 将到期的PublishBody迁移到ready队列等待执行
+func (op *RedisOperator) migrateExpiredBody(ctx context.Context) (err error) {
+
+	return
 }
 
 // PushDelayQueue 往延迟队列推送消息
