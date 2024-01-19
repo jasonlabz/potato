3543fd79cd544ba1ce054e9a5d06376eb7034dd2
glus <1783022886@qq.com>
2024-01-19 22:15:33 +0800

    Update es_test.go

--- es/es_test.go
+++ es/es_test.go
@@ -1,12 +1,14 @@
 package es
 
 import (
+	"context"
 	"fmt"
 	"testing"
 )
 
 func TestES(t *testing.T) {
-	cli, err := NewESClient(&Config{
+	ctx := context.Background()
+	cli, err := NewElasticSearchOperator(&Config{
 		Endpoints: []string{"127.0.0.1:9200"},
 		Username:  "elastic",
 		Password:  "openthedoor",
@@ -15,4 +17,13 @@ func TestES(t *testing.T) {
 		panic(err)
 	}
 	fmt.Println(cli)
+	err = cli.CreateIndex(ctx, "test1", `{"mappings":{"_doc":{"_all":{"enabled":false},"properties":{"uuid":{"type":"text","copy_to":"_search_all","fields":{"keyword":{"type":"keyword","ignore_above":150}}},"name":{"type":"text","copy_to":"_search_all","analyzer":"ik_max_word","search_analyzer":"ik_smart","fields":{"keyword":{"type":"keyword","ignore_above":150}}},"dt_from_explode_time":{"type":"date","copy_to":"_search_all","format":"strict_date_optional_time||epoch_millis"},"_search_all":{"type":"text"}},"date_detection":false,"dynamic_templates":[{"strings":{"match_mapping_type":"string","mapping":{"type":"text","copy_to":"_search_all","fields":{"keyword":{"type":"keyword","ignore_above":150}}}}}]}},"settings":{"index":{"number_of_shards":6,"number_of_replicas":1}}}`)
+	if err != nil {
+		return
+	}
+	list, err := cli.GetIndexList(ctx)
+	if err != nil {
+		return
+	}
+	fmt.Println(list)
 }
