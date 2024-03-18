cd2ddaf701ac99248e0953b0fd2a14326f00c09b
zhouzhigao01 <zhouzhigao01@baidu.com>
2024-03-19 00:15:25 +0800

    update const

--- core/consts/context.go
+++ core/consts/context.go
@@ -4,11 +4,11 @@ const (
 	HeaderRequestID     = "X-RequestID"
 	HeaderAuthorization = "X-Authorization"
 	HeaderUserID        = "X-UserID"
-	HeaderRemote        = "X-Remote"
+	HeaderRemote        = "X-Client"
 
 	ContextTraceID    = "trace_id"
 	ContextUserID     = "user_id"
 	ContextSpanID     = "span_id"
 	ContextToken      = "token"
-	ContextRemoteAddr = "remote_addr"
+	ContextClientAddr = "client_addr"
 )
