68feca3f24da84dd87c44d514a1bc19f94bd65ec
lucas <1783022886@qq.com>
2025-04-17 01:45:54 +0800

    config

--- configx/config.go
+++ configx/config.go
@@ -2,7 +2,6 @@ package configx
 
 import (
 	"fmt"
-
 	"github.com/spf13/cast"
 
 	"github.com/jasonlabz/potato/configx/base"
@@ -28,19 +27,83 @@ func Search(key string) any {
 }
 
 func SearchE(key string) (any, error) {
-	var v any
-	pm.ProviderMap.Range(func(key, value any) bool {
-		val, err := value.(base.IProvider).Get(key.(string))
-		if err != nil {
-			return true
-		}
-		v = val
-		return false
-	})
+	var v = Search(key)
 	if v == nil {
 		return nil, fmt.Errorf("key[%s] not found", key)
 	}
 	return v, nil
+}
+
+func SearchInt(key string) int {
+	return cast.ToInt(Search(key))
+}
+
+func SearchIntE(key string) (int, error) {
+	return cast.ToIntE(Search(key))
+}
+
+func SearchInt32(key string) int32 {
+	return cast.ToInt32(Search(key))
+}
+
+func SearchInt32E(key string) (int32, error) {
+	return cast.ToInt32E(Search(key))
+}
+
+func SearchInt64(key string) int64 {
+	return cast.ToInt64(Search(key))
+}
+
+func SearchInt64E(key string) (int64, error) {
+	return cast.ToInt64E(Search(key))
+}
+
+func SearchString(key string) string {
+	return cast.ToString(Search(key))
+}
+
+func SearchStringE(key string) (string, error) {
+	return cast.ToStringE(Search(key))
+}
+
+func SearchBool(key string) bool {
+	return cast.ToBool(Search(key))
+}
+
+func SearchBoolE(key string) (bool, error) {
+	return cast.ToBoolE(Search(key))
+}
+
+func SearchIntSlice(key string) []int {
+	return cast.ToIntSlice(Search(key))
+}
+
+func SearchIntSliceE(key string) ([]int, error) {
+	return cast.ToIntSliceE(Search(key))
+}
+
+func SearchStringSlice(key string) []string {
+	return cast.ToStringSlice(Search(key))
+}
+
+func SearchStringSliceE(key string) ([]string, error) {
+	return cast.ToStringSliceE(Search(key))
+}
+
+func SearchStringMap(key string) map[string]interface{} {
+	return cast.ToStringMap(Search(key))
+}
+
+func SearchStringMapE(key string) (map[string]string, error) {
+	return cast.ToStringMapStringE(Search(key))
+}
+
+func SearchStringMapString(key string) map[string]interface{} {
+	return cast.ToStringMap(Search(key))
+}
+
+func SearchStringMapStringE(key string) (map[string]string, error) {
+	return cast.ToStringMapStringE(Search(key))
 }
 
 func Get(configName, key string) interface{} {
