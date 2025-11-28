ee5f063cab620e3a1a3259376b7c689b1dec5843
glus <1783022886@qq.com>
2025-11-28 16:45:25 +0800

    Update body.go

--- es/body.go
+++ es/body.go
@@ -1,5 +1,14 @@
 package es
 
 type IndexInfo struct {
-	Name string `json:"index"`
+	Name         string `json:"index"`
+	Health       string `json:"health"`
+	Status       string `json:"status"`
+	UUID         string `json:"uuid"`
+	Pri          string `json:"pri"`
+	Rep          string `json:"rep"`
+	Count        string `json:"docs.count"`
+	Deleted      string `json:"docs.deleted"`
+	StoreSize    string `json:"store.size"`
+	PriStoreSize string `json:"pri.store.size"`
 }
